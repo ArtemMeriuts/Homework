d8f0e0b98ce9a582fc5f742c22268fc7a798a06a
artemmer <art.meriuts@gmail.com>
2022-05-14 18:20:54 +0200

    Initial commit

---
+++ src/main/java/Dz6.java
@@ -0,0 +1,19 @@
+public class Dz6 {
+    //| a- b | / (a + b) ^3 - √c
+
+
+    public static void math () {
+
+
+        double f= Math.abs(5-7)/(5+7)*Math.pow(3,2)-Math.sqrt(9);
+        System.out.println(f);
+    }
+
+
+    public static void main(String[] args) {
+     Dz6.math();
+    }
+
+
+}
+
